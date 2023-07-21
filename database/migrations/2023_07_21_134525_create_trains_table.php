fd68609f66b6c2e46697c87b09be10849a311658
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-21 16:12:45 +0200

    created columns into trains table

---
+++ database/migrations/2023_07_21_134525_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 50);
+            $table->string('Stazione di arrivo', 50);
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->smallInteger('Codice Treno');
+            $table->tinyInteger('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
