06771941e56b9a5d77a2908f062e863f1fa17ae8
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-21 16:31:59 +0200

    added Train to Models folder & created TrainSeeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $newTrain = new Train();
+
+    }
+}
